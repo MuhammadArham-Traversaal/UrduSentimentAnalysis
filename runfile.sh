8fcb15259863d93a4e98684be6650916331ca769
Muhammad Arham <muhammad.arham@traversaal.ai>
2024-10-24 22:04:18 +0500

    Fix Errors

--- runfile.sh
+++ runfile.sh
@@ -1,4 +1,4 @@
-python main.py --model unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit > outputs/urdu_llama3-1.log
-python main.py --model traversaal-llm-regional-languages/Unsloth_Urdu_Llama3_1_4bit_PF100 > outputs/unsloth_llama3-1.log
+python main.py --model unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit > outputs/unsloth_llama3-1.log
+python main.py --model traversaal-llm-regional-languages/Unsloth_Urdu_Llama3_1_4bit_PF100 > outputs/urdu_llama3-1.log
 python main.py --model unsloth/Llama-3.2-1B-Instruct-bnb-4bit > outputs/unsloth_llama3-2.log
 python main.py --model traversaal-llm-regional-languages/Urdu_Llama3_2_4bit_PF25_adapter > outputs/urdu_llama3-2.log
